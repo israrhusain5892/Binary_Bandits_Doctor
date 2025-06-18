18c66979daa6ec26eefb9c9405d81bfa564470fe
israrhusain5892 <israrhusain5892@gmail.com>
2025-06-18 19:21:01 +0530

    connect db

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,31 +4,22 @@ import { AppService } from './app.service';
 import { HelloWorldModule } from './hello-world/hello-world.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
 
   imports: [
-     ConfigModule.forRoot({
-      isGlobal: true, // <== make env variables available globally
-    }),
     TypeOrmModule.forRoot({
-      imports: [ConfigModule], // ✅ THIS LINE is valid
-      inject: [ConfigService],
-      useFactory: async (config: ConfigService) => ({
-        type: 'postgres',
-        host: config.get<string>('DB_HOST'),
-        port: config.get<number>('DB_PORT'),
-        username: config.get<string>('DB_USER'),
-        password: config.get<string>('DB_PASS'),
-        database: config.get<string>('DB_NAME'),
-        entities: [],
-        synchronize: true,
-     
-      }),
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'your_db_password',
+      database: 'auth_db',
+      entities: [],
+      synchronize: true,
     }),
     HelloWorldModule, AuthModule
-  ]
-  ,
+  ],
+  
   controllers: [AppController],
   providers: [AppService],
 })
